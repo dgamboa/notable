2e88568012950879a3a48a4c5c21099b07829551
Daniel Gamboa <danielgamboa10@gmail.com>
2012-11-25 19:28:01 -0800

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Notable::Application.routes.draw do
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
+
+  resources :users
+  resources :sessions
+
+  root :to => "users#index"
+  # root to: "topics#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121125233529) do
+ActiveRecord::Schema.define(:version => 20121126003622) do
 
   create_table "notes", :force => true do |t|
     t.text     "body"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(:version => 20121125233529) do
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
